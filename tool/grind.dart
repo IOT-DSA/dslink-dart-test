eb961cd26384cb09a3843ac3c9a689ab5b4e8b15
Joel Trottier-Hebert <iamquebecois@gmail.com>
2016-11-10 11:03:23 -0500

    Make output continous in console

--- tool/grind.dart
+++ tool/grind.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:grinder/grinder.dart';
 import 'dart:io';
 import 'package:path/path.dart' as path;
+import 'dart:convert';
 
 final String etsdbLinkName = 'dslink-java-etsdb-0.16.0-SNAPSHOT';
 final String etsdbRepositoryUrl =
@@ -22,50 +23,56 @@ Future<Directory> cloneGitRepository(String repositoryUrl,
   var cloneDirectory =
       await new Directory(projectPath).createTemp(directoryPrefix);
 
-  var process = await Process.run(
+  var process = await Process.start(
       'git', ['clone', '-b', branchName, repositoryUrl, cloneDirectory.path]);
 
-  printProcessOutput(process);
+  await printProcessOutput(process);
 
   return cloneDirectory;
 }
 
-void printProcessOutput(ProcessResult process) {
-  print(process.stdout);
-  print(process.stderr);
+Future<Null> printProcessOutput(Process p) async {
+  p.stderr.listen((List<int> out) => print(new Utf8Decoder().convert(out)));
+  p.stdout.listen((List<int> out) => print(new Utf8Decoder().convert(out)));
 
-  if (process.exitCode != 0) {
+  if (await p.exitCode != 0) {
     throw 'Build step failed';
   }
 }
 
 Future<Null> buildJavaLink(Directory linkDirectory) async {
   print('********* Building Java Link ***********');
-  var process = await Process.run('gradle', ['clean', 'build', 'distZip'],
+  var process = await Process.start('gradle', ['clean', 'build', 'distZip'],
       workingDirectory: linkDirectory.path);
 
-  printProcessOutput(process);
+  await printProcessOutput(process);
+
+  await process.exitCode;
 }
 
 Future<Null> buildJavaSdk(Directory linkDirectory) async {
   print('********* Building Java SDK ***********');
-  var process = await Process.run('gradle', ['build'],
+  var process = await Process.start('gradle', ['build'],
       workingDirectory: linkDirectory.path);
 
-  printProcessOutput(process);
+  await printProcessOutput(process);
+
+  await process.exitCode;
 }
 
 Future<Directory> dumpDistZip(Directory linkDirectory, String linkName) async {
   print('********* Dumping Link DistZip ***********');
   var dumpDirectory = await new Directory(projectPath).createTemp('dump-');
 
-  var process = await Process.run('unzip', [
+  var process = await Process.start('unzip', [
     '${linkDirectory.path}/build/distributions/$linkName',
     '-d',
     dumpDirectory.path
   ]);
 
-  printProcessOutput(process);
+  await printProcessOutput(process);
+
+  await process.exitCode;
 
   return dumpDirectory;
 }
@@ -96,12 +103,12 @@ Future<Null> repackageEtsdb({String sdkBranchName: 'master'}) async {
 Future<File> zipRepackagedLink(
     Directory linkDumpDirectory, File outputFile) async {
   print('********* Zipping updated link ***********');
-  var process = await Process.run('zip', ['-r', outputFile.path, etsdbLinkName],
+  var process = await Process.start('zip', ['-r', outputFile.path, etsdbLinkName],
       workingDirectory: linkDumpDirectory.path);
 
-  printProcessOutput(process);
+  await printProcessOutput(process);
 
-  if (process.exitCode != 0) {
+  if (await process.exitCode != 0) {
     throw 'Build step failed';
   }
   return outputFile;

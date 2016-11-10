c4282ce52bc4c415ba02479ab05a950aec59aed7
Joel Trottier-Hebert <iamquebecois@gmail.com>
2016-11-10 12:34:30 -0500

    dartfmt

--- lib/test_broker_node_provider.dart
+++ lib/test_broker_node_provider.dart
@@ -3,21 +3,20 @@ import 'package:dslink/responder.dart';
 import 'dart:async';
 
 class TestBrokerNodeProvider extends BrokerNodeProvider {
-  TestBrokerNodeProvider({
-    enabledQuarantine: false,
-    acceptAllConns: true,
-    defaultPermission,
-    downstreamName: "conns",
-    IStorageManager storage,
-    enabledDataNodes: true
-  }) : super(
-      enabledQuarantine: enabledQuarantine,
-      acceptAllConns: acceptAllConns,
-      defaultPermission: defaultPermission,
-      downstreamName: downstreamName,
-      storage: storage,
-      enabledDataNodes: enabledDataNodes
-  );
+  TestBrokerNodeProvider(
+      {enabledQuarantine: false,
+      acceptAllConns: true,
+      defaultPermission,
+      downstreamName: "conns",
+      IStorageManager storage,
+      enabledDataNodes: true})
+      : super(
+            enabledQuarantine: enabledQuarantine,
+            acceptAllConns: acceptAllConns,
+            defaultPermission: defaultPermission,
+            downstreamName: downstreamName,
+            storage: storage,
+            enabledDataNodes: enabledDataNodes);
 
   @override
   Future<Map> saveConns() async {
